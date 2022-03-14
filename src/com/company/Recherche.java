f2e0c962b10b0255252998242e818d738cac2f60
LaZoRBear <lainesse.maxime@hotmail.com>
2022-03-13 22:54:50 -0400

    Recherche.java
    ADDED test to create a DB and table. Cleanup DB at start and end.

--- src/com/company/Recherche.java
+++ src/com/company/Recherche.java
@@ -41,10 +41,27 @@ public class Recherche {
         ResultSetMetaData responseMeta;
         Statement statement;
         try {
+            String sql;
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pokedex", "postgres", "m#S73y%7");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "m#S73y%7");
             statement = conn.createStatement();
-            String sql = "SELECT * FROM pokemon ORDER BY national_dex ASC LIMIT 5;";
+            sql = "SELECT datname FROM pg_database WHERE datname = 'test';";
+            response = statement.executeQuery(sql);
+            if (response.next()) {
+                System.out.println(response.getString(1));
+                sql = "DROP DATABASE test;";
+                statement.executeUpdate(sql);
+            }
+            sql = "CREATE DATABASE test;";
+            statement.execute(sql);
+            conn.close();
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "m#S73y%7");
+            statement = conn.createStatement();
+            sql = "CREATE TABLE test (ID INT NOT NULL, NAME VARCHAR(255) NOT NULL);";
+            statement.executeUpdate(sql);
+            sql = "INSERT INTO test VALUES (1, 'Maxime');";
+            statement.executeUpdate(sql);
+            sql = "SELECT * FROM test;";
             response = statement.executeQuery(sql);
             responseMeta = response.getMetaData();
             while (response.next()) {
@@ -53,6 +70,11 @@ public class Recherche {
                 }
                 System.out.println("--------------------------------------------------");
             }
+            conn.close();
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "m#S73y%7");
+            statement = conn.createStatement();
+            sql = "DROP DATABASE test;";
+            statement.executeUpdate(sql);
             statement.close();
         } catch (Exception e) {
             e.printStackTrace();
