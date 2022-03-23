d5277327d1036bb88e8b3f1b09cf3711ab001096
LainesseM-BdeB <0684941@bdeb.qc.ca>
2022-03-22 23:09:47 -0400

    CLEANUP Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,7 +27,6 @@ public class Main {
             mainM.printMenu();
             System.out.println("Choisissez une des options:");
             input = inputU.nextLine().toLowerCase().strip();
-            //System.out.println(System.lineSeparator().repeat(5));
             switch (input) {
                 case "q" -> exit = true;
                 case "1" -> traMenu(bibli);
@@ -44,116 +43,6 @@ public class Main {
         bibli.unloadBottin();
         bibli.unloadTracker();
         //Live code for menu DO NOT DELETE OR CHANGE ^^^^^
-
-
-        //TEST POSTGRESQL
-        //Recherche pg = new Recherche();
-        //pg.testPostgresql();
-        //FIN TEST POSTGRESQL
-
-
-        //Testing des classes
-        //LocalDate today = LocalDate.now();
-        //System.out.println(today);
-        //System.out.println(today.plusDays(14));
-        //today = today.minusDays(2);
-        //System.out.println(today.getDayOfWeek());
-        //System.out.println(today.getDayOfWeek().getValue());
-        //Map<Integer, String> semaine = new HashMap<>(7) {{
-        //    put(1, "Lundi");
-        //    put(2, "Mardi");
-        //    put(3, "Mercredi");
-        //    put(4, "Jeudi");
-        //    put(5, "Vendredi");
-        //    put(6, "Samedi");
-        //    put(7, "Dimanche");
-        //}};
-        //System.out.println(semaine.get(today.getDayOfWeek().getValue()));
-        //
-        //
-        //bibli.loadInventaire();
-        //bibli.addDocument(new Livre("Java pour les nuls", "Maxime Lainesse"));
-        //bibli.addDocument(new BD("Java pour les nuls en dessins!", "Maxime Lainesse", "Vincent Poirier"));
-        //bibli.addDocument(new Journal("Le journal de Baie-Comeau", LocalDate.now()));
-        //bibli.addDocument(new OuvrageReference("L'atlas des patates", "Madame Brossard", "Alimentation"));
-        //
-        //bibli.showInventaire();
-        //
-        //bibli.addDocument(new BD("Tintin et le point-virgule manquant", "Hergé", "Hergé"));
-        //
-        //bibli.showInventaire();
-        //
-        //boolean good = false;
-        //do {
-        //    Scanner inputU = new Scanner(System.in);
-        //    System.out.println("Choisissez un des livres:");
-        //    int i = inputU.nextInt();
-        //    int max = bibli.getInventaire().size();
-        //    if (i > max || i < 1) {
-        //        System.out.println("Le chiffre entré n'est pas valide.");
-        //    } else {
-        //        bibli.getInventaire().get(i - 1).printInfo();
-        //        good = true;
-        //    }
-        //} while (!good);
-        //
-        //bibli.unloadInventaire();
-        //
-        //System.out.println("###################################################################");
-        //for (Document doc : bibli.getInventaire()) {
-        //    doc.printInfo();
-        //}
-        //System.out.println("###################################################################");
-        //
-        //Recherche response = bibli.getDocument("D3-J1");
-        //if (response.getFound()) {
-        //    bibli.getInventaire().get(response.getDocument()).printInfo();
-        //    bibli.removeDocument(response.getDocument());
-        //} else {
-        //    System.out.println("There wasn't any Document with this ID");
-        //}
-        //
-        //System.out.println("###################################################################");
-        //for (Document doc : bibli.getInventaire()) {
-        //    doc.printInfo();
-        //}
-        //System.out.println("###################################################################");
-
-
-        //
-        //System.out.println();
-        //
-        //
-        //ArrayList<Document> inventaire = new ArrayList<>();
-        //inventaire.add(new Livre("Java pour les nuls", "Maxime Lainesse"));
-        //inventaire.add(new BD("Java pour les nuls en dessins!", "Maxime Lainesse", "Vincent Poirier"));
-        //
-        //for (Document doc : inventaire) {
-        //    doc.printInfo();
-        //    System.out.println("==============================");
-        //}
-
-        // test de la classe Membre
-
-        //Map<String, Member> members = new HashMap<>();
-        //Member tempMemb = new Member("César", "Jules");
-        //members.put(tempMemb.getMemID(), tempMemb);
-        //System.out.println();
-        //
-        //Map<String, Emprunt> emprunts = new HashMap<>();
-        //Emprunt tempEmp = new Emprunt("01-01-2022", "16-01-2202","1CÉS","RR");
-        //emprunts.put(tempEmp.getEmp(), tempEmp);
-        //System.out.println();
-
-
-        //for ( Member ls : members) {
-
-        //   ls.afficher();
-        //    System.out.println("_____________________");
-
-        //}
-
-        //Fin testing des classes
     }
 
     private static void invMenu(Bibliotheque bibli) throws Exception {
